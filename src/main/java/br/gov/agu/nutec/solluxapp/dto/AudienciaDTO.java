1613c7d35d95f5b0c5528e4f4bcdd81b95f26b2f
Danilo Chaves <danchavesds@gmail.com>
2025-09-11 20:20:02 -0300

    Adicionando record para audiencia

---
+++ src/main/java/br/gov/agu/nutec/solluxapp/dto/AudienciaDTO.java
@@ -0,0 +1,22 @@
+package br.gov.agu.nutec.solluxapp.dto;
+
+import br.gov.agu.nutec.solluxapp.enums.Turno;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record AudienciaDTO(
+        String cnj,
+        LocalDate data,
+        Turno turno,
+        String sala,
+        String orgaoJulgador,
+        String poloAtivo,
+        String poloPassivo,
+        String classeJudicial,
+        List<String> advogados,
+        String assunto,
+        String tipo,
+        String situacao
+) {
+}
