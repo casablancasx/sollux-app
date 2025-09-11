9d38996cbfc4ef2d26194b9cb1384aee992ce650
Danilo Chaves <danchavesds@gmail.com>
2025-09-11 19:53:56 -0300

    commit inicial

---
+++ src/main/java/br/gov/agu/nutec/solluxapp/SolluxAppApplication.java
@@ -0,0 +1,13 @@
+package br.gov.agu.nutec.solluxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolluxAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolluxAppApplication.class, args);
+    }
+
+}
