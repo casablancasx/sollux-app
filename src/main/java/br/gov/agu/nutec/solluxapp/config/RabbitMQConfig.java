a062c0f43e927682781dde228e45d35d8445a5b0
Danilo Chaves <danchavesds@gmail.com>
2025-09-12 12:41:08 -0300

    Adicionando configuração do RabbitMQ e implementando filas e exchanges para a comunicação assíncrona

--- src/main/java/br/gov/agu/nutec/solluxapp/config/RabbitMQConfig.java
+++ src/main/java/br/gov/agu/nutec/solluxapp/config/RabbitMQConfig.java
@@ -1,6 +1,14 @@
 package br.gov.agu.nutec.solluxapp.config;
 
-import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,13 +16,65 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
 
+    @Value("${rabbitmq.exchange.audiencia-pendente}")
+    private String exchangeAudienciaPendente;
+
+    @Value("${rabbitmq.queue.sollux-alchemy}")
+    private String filaAudienciaPendenteMsAlchemy;
+
+    @Value("${rabbitmq.queue.sollux-pace}")
+    private String filaAudienciaPendenteMsPace;
 
 
-@Bean
-    public FanoutExchange exchange() {
-        return new FanoutExchange("planilhaExchange");
-}
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
 
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarRabbitAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+
+    @Bean
+    public FanoutExchange fanoutExchange(){
+        return ExchangeBuilder.fanoutExchange(exchangeAudienciaPendente).build();
+    }
+
+
+    @Bean
+    public Queue filaAudienciaPendenteMsAlchemy(){
+        return QueueBuilder.durable(filaAudienciaPendenteMsAlchemy).build();
+    }
+
+    @Bean
+    public Queue filaAudienciaPendenteMsPace(){
+        return QueueBuilder.durable(filaAudienciaPendenteMsPace).build();
+    }
+
+    @Bean
+    public Binding bindingFilaAudienciaPendenteMsAlchemy(FanoutExchange fanoutExchange, Queue filaAudienciaPendenteMsAlchemy){
+        return BindingBuilder.bind(filaAudienciaPendenteMsAlchemy).to(fanoutExchange);
+    }
+
+    @Bean
+    public Binding bindingFilaAudienciaPendenteMsPace(FanoutExchange fanoutExchange, Queue filaAudienciaPendenteMsPace){
+        return BindingBuilder.bind(filaAudienciaPendenteMsPace).to(fanoutExchange);
+    }
 
 
 }
